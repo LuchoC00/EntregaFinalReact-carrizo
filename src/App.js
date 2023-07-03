125e409798718f1c9703035e433469fc068ac187
Luciano <raulo.carri@gmail.com>
2023-07-03 18:30:31 -0300

    paginas añadidas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <p style={{textAlign:"center"}}>hello world</p>
+      <Router>
+        <Routes>
+          <Route path="/" element={''} />
+          <Route path="/search" element={''} />
+          <Route path="/cart" element={''} />
+          <Route path="/login" element={''} />
+          <Route path="/register" element={''} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/contexts/ProductsContext.js
@@ -0,0 +1,7 @@
+import { createContext, useState, useEffect } from 'react';
+
+const ProductsContext = () => {
+  return <div>ProductsContext</div>;
+};
+
+export default ProductsContext;
